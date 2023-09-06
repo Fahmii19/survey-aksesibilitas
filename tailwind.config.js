08a9b02aba8225343a6538a8667d11ef3c1e914e
Fahmii19 <fhmialfi79@gmail.com>
2023-09-06 17:20:50 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}", "./node_modules/flowbite/**/*.js"],
+  theme: {
+    screens: {
+      xs: "320px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    extend: {},
+  },
+  plugins: [require("flowbite/plugin")],
+};
