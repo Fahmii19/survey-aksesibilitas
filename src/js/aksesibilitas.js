ea28d964f9176d7be1e483afd65b96d1d606cffa
Fahmii19 <fhmialfi79@gmail.com>
2023-09-25 18:56:17 +0700

    uji coba

--- src/js/aksesibilitas.js
+++ src/js/aksesibilitas.js
@@ -1,190 +1,152 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // navigation
   const mapboxCtrlElement = document.querySelector(".mapboxgl-ctrl-top-right");
 
-  mapboxCtrlElement.style.zIndex = "5"; // Set default z-index
+  if (mapboxCtrlElement) {
+    mapboxCtrlElement.style.zIndex = "5"; // Set default z-index
+  }
 
   const formInputAkses = document.querySelector(".form-input-akses");
   const tableRekap = document.querySelector(".table-rekap-hide");
-  const expandRekap = document.querySelector(".expand_rekap");
-  const expandForm = document.querySelector(".expand_form_input");
-  const expandProfil = document.querySelector(".expand_profil");
   const btnRekap = document.querySelector(".btn_rekap");
-  const btnRekapImage = btnRekap.querySelector(".menu-image-rekap");
+  const btnRekapImage = btnRekap
+    ? btnRekap.querySelector(".menu-image-rekap")
+    : null;
   const btnInput = document.querySelector("#toggleButtonOpen");
   const formProfil = document.querySelector(".form-profil");
   const btnProfil = document.querySelector(".btn_profil");
-  const btnProfilImage = btnProfil.querySelector(".menu-image-profil");
-  const formContainerFormInput = formInputAkses;
-  const toggleButtonOpen = btnInput;
+  const btnProfilImage = btnProfil
+    ? btnProfil.querySelector(".menu-image-profil")
+    : null;
   const toggleButtonFormInput = document.getElementById(
     "toggleButtonFormInput"
   );
-  const formContainerProfil = formProfil;
   const toggleButtonProfil = document.getElementById("toggleButtonProfil");
-  const formContainerRekap = tableRekap;
-  const formContainerRekapHide = expandRekap;
-  const formContainerFormInputHide = expandForm;
-  const formContainerProfilHide = expandProfil;
   const toggleButtonRekap = document.getElementById("toggleButtonRekap");
+  const rekapContainer = document.getElementById("rekapContainer");
   const btnOpenTableDetail = document.querySelector(".btn_open_table_detail");
   const tableDetail = document.querySelector(".table-detail");
-
-  const hideToggleButtonRekap = document.querySelector("#toggleButtonRekap");
-
-  btnOpenTableDetail.addEventListener("click", function () {
-    tableDetail.classList.toggle("hidden");
-  });
-
-  // Reset Menu Navigation
-  function resetAll() {
-    tableRekap.classList.add("hidden");
-    formInputAkses.classList.add("hidden");
-    formProfil.classList.add("hidden");
-    tableRekap.style.zIndex = "1";
-    formInputAkses.style.zIndex = "1";
-    formProfil.style.zIndex = "1";
-    mapboxCtrlElement.style.zIndex = "5";
-    btnRekapImage.src = "./src/images/rekap.png";
-    btnProfilImage.src = "./src/images/user.png";
-  }
-
-  // btnRekap.addEventListener("click", function () {
-  //   resetAll();
-  //   tableRekap.classList.remove("hidden");
-  //   tableRekap.style.zIndex = "6";
-  //   btnRekapImage.src = "./src/images/active_rekap.png";
-  // });
-
-  btnRekap.addEventListener("click", function () {
-    if (tableRekap.classList.contains("hidden")) {
-      // Tampilkan elemen
-      tableRekap.classList.remove("hidden");
-      tableRekap.style.zIndex = "6";
-      btnRekapImage.src = "./src/images/active_rekap.png";
-    } else {
-      // Sembunyikan elemen
-      tableRekap.classList.add("hidden");
-      tableRekap.style.zIndex = "1"; // Anda bisa mengatur ke nilai default yang Anda inginkan.
-      btnRekapImage.src = "./src/images/rekap.png"; // Asumsi Anda memiliki gambar untuk keadaan non-aktif. Gantilah dengan path gambar yang sesuai.
-    }
-  });
-
-  btnInput.addEventListener("click", function () {
-    resetAll();
-    formInputAkses.classList.remove("hidden");
-    formInputAkses.style.zIndex = "6";
-  });
-
-  btnProfil.addEventListener("click", function () {
-    resetAll();
-    formProfil.classList.remove("hidden");
-    formProfil.style.zIndex = "6";
-    btnProfilImage.src = "./src/images/active_user.png";
-  });
-
-  // Mengganti 'display' dengan 'z-index'
-  function toggleHeight(element, initialHeight, toggledHeight) {
-    if (element.style.height === initialHeight || !element.style.height) {
-      element.style.height = toggledHeight;
-    } else {
-      element.style.height = initialHeight;
-    }
-  }
-
-  // Form Input
-  toggleButtonFormInput.addEventListener("click", function () {
-    toggleHeight(formContainerFormInput, "30vh", "74vh");
-    toggleHeight(formContainerFormInputHide, "19vh", "63vh");
-
-    // Mengganti 'display' dengan 'z-index'
-    if (formContainerFormInput.style.height === "74vh") {
-      mapboxCtrlElement.style.zIndex = "-1";
-    } else {
-      mapboxCtrlElement.style.zIndex = "5";
-    }
-  });
-
-  // Profil
-  toggleButtonProfil.addEventListener("click", function () {
-    toggleHeight(formContainerProfil, "30vh", "75vh");
-    toggleHeight(formContainerProfilHide, "19vh", "45vh");
-
-    // Mengganti 'display' dengan 'z-index'
-    if (formContainerProfil.style.height === "75vh") {
-      mapboxCtrlElement.style.zIndex = "-1";
-    } else {
-      mapboxCtrlElement.style.zIndex = "5";
-    }
-  });
-
-  // Rekap
-  toggleButtonRekap.addEventListener("click", function () {
-    toggleHeight(formContainerRekap, "30vh", "84vh");
-    toggleHeight(formContainerRekapHide, "20vh", "65vh");
-
-    // Mengganti 'display' dengan 'z-index'
-    if (formContainerRekap.style.height === "84vh") {
-      mapboxCtrlElement.style.zIndex = "-1";
-      hideToggleButtonRekap.style.display = "none";
-    } else {
-      mapboxCtrlElement.style.zIndex = "5";
-    }
-  });
-  //
   const collapseImage = document.getElementById("collapseImage");
-
-  btnOpenTableDetail.addEventListener("click", function () {
-    // Toggle table detail visibility
-    // tableDetail.classList.toggle("hidden");
-
-    // Toggle collapse icon
-    if (collapseImage.src.endsWith("open_collapse.png")) {
-      collapseImage.src = "./src/images/close_collapse.png";
-    } else {
-      collapseImage.src = "./src/images/open_collapse.png";
-    }
-  });
-
-  // Gesture scroll up untuk menampilkan konten
-
   const tableRekapElement = document.querySelector(".table-rekap-hide");
-  let touchStartY = 0;
-  let initialTableHeight = 30; // Dalam vh
 
-  tableRekapElement.style.height = initialTableHeight + "vh";
+  if (btnOpenTableDetail && tableDetail) {
+    btnOpenTableDetail.addEventListener("click", function () {
+      tableDetail.classList.toggle("hidden");
+      if (collapseImage && collapseImage.src.endsWith("open_collapse.png")) {
+        collapseImage.src = "./src/images/close_collapse.png";
+      } else if (collapseImage) {
+        collapseImage.src = "./src/images/open_collapse.png";
+      }
+    });
+  }
 
-  tableRekapElement.addEventListener("touchstart", function (event) {
-    touchStartY = event.touches[0].clientY;
-    initialTableHeight = parseFloat(tableRekapElement.style.height);
-    // Menghapus transisi saat menyeret untuk mendapatkan respons langsung
-    tableRekapElement.style.transition = "none";
-  });
+  function resetAll() {
+    if (tableRekap) tableRekap.classList.add("hidden");
+    if (formInputAkses) formInputAkses.classList.add("hidden");
+    if (formProfil) formProfil.classList.add("hidden");
+    if (mapboxCtrlElement) mapboxCtrlElement.style.zIndex = "5";
+    if (btnRekapImage) btnRekapImage.src = "./src/images/rekap.png";
+    if (btnProfilImage) btnProfilImage.src = "./src/images/user.png";
+  }
 
-  tableRekapElement.addEventListener("touchmove", function (event) {
-    const touchCurrentY = event.touches[0].clientY;
-    const diffY = ((touchStartY - touchCurrentY) / window.innerHeight) * 100; // Menghitung perbedaan dalam vh
+  if (btnRekap && tableRekap && btnRekapImage) {
+    btnRekap.addEventListener("click", function () {
+      if (tableRekap.classList.contains("hidden")) {
+        resetAll();
+        tableRekap.classList.remove("hidden");
+        btnRekapImage.src = "./src/images/active_rekap.png";
+      } else {
+        tableRekap.classList.add("hidden");
+        btnRekapImage.src = "./src/images/rekap.png";
+      }
+    });
+  }
 
-    let newHeight = initialTableHeight + diffY;
-    newHeight = Math.min(Math.max(30, newHeight), 84); // Memastikan tidak kurang dari 30 dan tidak lebih dari 80
+  if (btnInput && formInputAkses) {
+    btnInput.addEventListener("click", function () {
+      resetAll();
+      formInputAkses.classList.remove("hidden");
+    });
+  }
 
-    tableRekapElement.style.height = newHeight + "vh";
-  });
+  if (btnProfil && formProfil && btnProfilImage) {
+    btnProfil.addEventListener("click", function () {
+      resetAll();
+      formProfil.classList.remove("hidden");
+      btnProfilImage.src = "./src/images/active_user.png";
+    });
+  }
 
-  tableRekapElement.addEventListener("touchend", function () {
-    // Mengembalikan transisi saat selesai menyeret
-    tableRekapElement.style.transition = "height 0.3s"; // Anda bisa mengatur durasi sesuai kebutuhan
-
-    // Jika tinggi lebih dari 40vh setelah menyeret, perluas ke 80vh
-    if (parseFloat(tableRekapElement.style.height) > 55) {
-      tableRekapElement.style.height = "84vh";
-      hideToggleButtonRekap.style.display = "none";
-    } else {
-      // Jika tidak, kembalikan ke 30vh
-      tableRekapElement.style.height = "30vh";
-      hideToggleButtonRekap.style.display = "block";
+  function toggleHeight(element, initialHeight, toggledHeight) {
+    if (element) {
+      if (element.style.height === initialHeight || !element.style.height) {
+        element.style.height = toggledHeight;
+      } else {
+        element.style.height = initialHeight;
+      }
     }
-  });
+  }
 
-  //
+  if (toggleButtonFormInput && formInputAkses) {
+    toggleButtonFormInput.addEventListener("click", function () {
+      toggleHeight(formInputAkses, "30vh", "74vh");
+      if (formInputAkses.style.height === "74vh" && mapboxCtrlElement) {
+        mapboxCtrlElement.style.zIndex = "-1";
+      } else if (mapboxCtrlElement) {
+        mapboxCtrlElement.style.zIndex = "5";
+      }
+    });
+  }
+
+  if (toggleButtonProfil && formProfil) {
+    toggleButtonProfil.addEventListener("click", function () {
+      toggleHeight(formProfil, "30vh", "75vh");
+      if (formProfil.style.height === "75vh" && mapboxCtrlElement) {
+        mapboxCtrlElement.style.zIndex = "-1";
+      } else if (mapboxCtrlElement) {
+        mapboxCtrlElement.style.zIndex = "5";
+      }
+    });
+  }
+
+  if (toggleButtonRekap && rekapContainer) {
+    toggleButtonRekap.addEventListener("click", function () {
+      if (rekapContainer.style.height !== "90vh") {
+        rekapContainer.style.height = "90vh";
+      } else {
+        rekapContainer.style.height = "30vh";
+      }
+    });
+  }
+
+  if (tableRekapElement) {
+    tableRekapElement.addEventListener("touchstart", function (event) {
+      let touchStartY = event.touches[0].clientY;
+      let initialTableHeight = parseFloat(tableRekapElement.style.height);
+      tableRekapElement.style.transition = "none";
+
+      tableRekapElement.addEventListener("touchmove", function (event) {
+        const touchCurrentY = event.touches[0].clientY;
+        const diffY =
+          ((touchStartY - touchCurrentY) / window.innerHeight) * 100;
+        let newHeight = initialTableHeight + diffY;
+        newHeight = Math.min(Math.max(30, newHeight), 84);
+        tableRekapElement.style.height = newHeight + "vh";
+      });
+
+      tableRekapElement.addEventListener("touchend", function () {
+        tableRekapElement.style.transition = "height 0.3s";
+        const hideToggleButtonRekap =
+          document.querySelector("#toggleButtonRekap");
+        if (parseFloat(tableRekapElement.style.height) > 55) {
+          tableRekapElement.style.height = "84vh";
+          if (hideToggleButtonRekap)
+            hideToggleButtonRekap.style.display = "none";
+        } else {
+          tableRekapElement.style.height = "30vh";
+          if (hideToggleButtonRekap)
+            hideToggleButtonRekap.style.display = "block";
+        }
+      });
+    });
+  }
 });
